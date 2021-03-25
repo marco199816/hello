e25280a9a418e2f42c15bdd2a723202730bf5cc5
marco199816 <tshimangamarco@gmail.com>
2021-03-25 19:03:39 +0200

    first commit

---
+++ src/main/java/student.java
@@ -0,0 +1,16 @@
+public class student {
+    public static void main(String[] args) {
+        String name;
+        long studentNumber;
+        int age=12;
+        if (age ==11){
+            System.out.println("you are eleven");
+        }
+        else {
+            System.out.println("you not eleven");
+            System.out.println( Math.pow(2,5));
+
+        }
+
+    }
+}
